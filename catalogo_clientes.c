4d31e5d71dda00088ed5614b7ae85e9f7c53922e
Filipe Marques <filipemarques@Filipes-MBP.lan>
2016-04-15 23:47:27 +0100

    funcoes de travessia movidas para dentro do modulo catalogos

--- catalogo_clientes.c
+++ catalogo_clientes.c
@@ -1,9 +1,18 @@
-#include "avl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-typedef struct catalogoClientes {
+#include "avl.h"
+#include "venda.h"
+#include "navegacao.h"
+#include "catalogo_clientes.h"
+
+struct catalogoClientes {
     int totalClientes[27];
     Modulo indice[27];
-} *CatalogoClientes;
+};
 
 int calculaIndiceCliente(char l) {
     char letra = toupper(l);
@@ -17,16 +26,18 @@ int calculaIndiceCliente(char l) {
     return i;
 }
 
-int getTotalClientes(CatalogoClientes catalogo, int ind){
-    return catalogo->totalClientes[ind];
+Modulo getCatalogoClientesPorLetra(CatalogoClientes catalogo, char l) {
+    return catalogo->indice[calculaIndiceCliente(l)];
 }
 
-#include "navegacao.c"
+int getTotalClientesPorIndice(CatalogoClientes catalogo, int ind){
+    return catalogo->totalClientes[ind];
+}
 
 int calcularTotalClientes(CatalogoClientes catalogo){
     int i, somatorio = 0;
     for(i = 0; i <= 26; i++)
-        somatorio += getTotalClientes(catalogo, i);
+        somatorio += getTotalClientesPorIndice(catalogo, i);
     return somatorio;
 }
 
@@ -96,4 +107,50 @@ CodigoCliente_st inserirClienteCatalogo(CatalogoClientes catalogo, CodigoCliente
 	else freeCodigo(novo); //caso ja exista fazer free
 
 	return c;
+}
+
+PAGINA_RESULTADOS travessiaClientesPorLetra(CatalogoClientes catalogo, char letra){
+    int i = calculaIndiceCliente(toupper(letra));
+    int totalResultados = getTotalClientesPorIndice(catalogo,i);
+
+    TravessiaModulo trav = avl_trav_alloc();
+    avl_t_init(trav, getCatalogoClientesPorLetra(catalogo,letra));
+
+    CodigoCliente_st cliente = avl_t_next(trav);
+    PAGINA_RESULTADOS pagina = paginaResultadosInit(totalResultados,0);
+    inserirResultadoLista(pagina, cliente); //a inserir enderecos dos codigos (sem malloc)
+
+    int n = 0;
+    // totalResultados = 5;
+    while((cliente = avl_t_next(trav)) && n < totalResultados){
+        inserirResultadoLista(pagina, cliente);
+        n++;
+    }
+
+    printf("Total clientes começados por %c: %d\n", toupper(letra), getTotalClientesPorIndice(catalogo, i));
+    avl_trav_free(trav); //free
+
+    return pagina;
+}
+
+void funcaoImpressao(void *s){
+    printf("| %s |\n", (char *) s); //aqui faco gato sapato da cena
+}
+
+void travessiaTesteClientes(CatalogoClientes catalogo){
+    char c;
+    scanf("%c",&c);
+
+    PAGINA_RESULTADOS pagina = travessiaClientesPorLetra(catalogo, c);
+
+    percorrerPaginaResultados(pagina, 1, ELEM_POR_PAG, funcaoImpressao);
+
+    printf("Total clientes começados por %c: %d\n", toupper(c), getTotalClientesPorIndice(catalogo, calculaIndiceCliente(c)));
+}
+
+void freeTravessiaCatalogoClientes(TravessiaModulo travessia){
+    if(travessia != NULL)
+        avl_trav_free(travessia);
+
+    free(travessia);
 }
