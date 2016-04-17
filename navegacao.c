836cdb184b43d2315a9b0549ca0c5d9edd806f62
Filipe Marques <filipemarques@Filipes-MBP.lan>
2016-04-17 03:55:53 +0100

    funcao de ordenacao, novas variaveis dentro da estrutura, funcoes para voltar uma pagina para tras, funcao free estrutura, algoritmo de travessia diferente(sem necessidade da funcao de impressao)

--- navegacao.c
+++ navegacao.c
@@ -9,23 +9,20 @@
 #include "navegacao.h"
 #include "catalogo_clientes.h"
 
-// FALTA FAZER FUNCAO DE ORDENACAO
-
 struct paginaResultados{
     Listagem resultados;
     int tamanhoLista;
-    int tamanhoCadaElemento; //EM BYTES
-    int nrCampos;
+    int tamanhoCadaElemento; //EM BYTES - APAGAR ISTO
+    int nrCampos; // APAGAR ISTO
     int indiceAtual; //so para insercoes
-    int tempos;
-
-    int* tamanhoCampos;// meter aqui um array com o tamanho de cada um dos campos
+    int tempos; //APAGAR ISTO
     
     int nrPaginaAtual;
     int nrPaginasTotal;
     int nrElementosPorPagina;
     int posicaoInicial;
     int posicaoFinal;
+    int posicaoAtual;
 };
 
 int getTamanhoLista(PAGINA_RESULTADOS pagina){
@@ -34,6 +31,10 @@ int getTamanhoLista(PAGINA_RESULTADOS pagina){
 
 int getTamanhoCadaElemento(PAGINA_RESULTADOS pagina){
 	return pagina->tamanhoCadaElemento;
+}
+
+void** getListagem(PAGINA_RESULTADOS pagina){
+	return pagina->resultados;
 }
 
 void* getnElemento(PAGINA_RESULTADOS pagina, int n){
@@ -48,12 +49,20 @@ int getPaginaAtual(PAGINA_RESULTADOS pagina){
 	return pagina->nrPaginaAtual;
 }
 
+int getNrPaginasTotal(PAGINA_RESULTADOS pagina){
+	return pagina->nrPaginasTotal;
+}
+
 int getPosicaoInicial(PAGINA_RESULTADOS pagina){
 	return pagina->posicaoInicial;
 }
 
 int getPosicaoFinal(PAGINA_RESULTADOS pagina){
 	return pagina->posicaoFinal;
+}
+
+int getPosicaoAtual(PAGINA_RESULTADOS pagina){
+	return pagina->posicaoAtual;
 }
 
 int getNrElementosPorPagina(PAGINA_RESULTADOS pagina){
@@ -75,16 +84,32 @@ void setPosicaoFinal(PAGINA_RESULTADOS pagina, int pos){
 	pagina->posicaoFinal = pos;
 }
 
+void setPosicaoAtual(PAGINA_RESULTADOS pagina, int pos){
+	pagina->posicaoAtual = pos;
+}
+
 void setPaginaAtual(PAGINA_RESULTADOS pagina, int pag){
 	pagina->nrPaginaAtual = pag;
+}
+
+void setNrPaginasTotal(PAGINA_RESULTADOS pagina, int pag){
+	pagina->nrPaginasTotal = pag;
 }
 
 int incIndiceAtual(PAGINA_RESULTADOS pagina){
 	return pagina->indiceAtual++;
 }
 
+int incPosicaoAtual(PAGINA_RESULTADOS pagina){
+	return pagina->posicaoAtual++;
+}
+
 int incPaginaAtual(PAGINA_RESULTADOS pagina){
 	return pagina->nrPaginaAtual++;
+}
+
+int decPaginaAtual(PAGINA_RESULTADOS pagina){
+	return pagina->nrPaginaAtual--;
 }
 
 int calcularIndiceInsercao(PAGINA_RESULTADOS pagina){
@@ -103,8 +128,19 @@ PAGINA_RESULTADOS paginaResultadosInit(int tamanhoL, int tamanhoElemento){
 	pagina->nrElementosPorPagina = 0;
 	pagina->posicaoInicial = 0;
 	pagina->posicaoFinal = 0;
+	pagina->posicaoAtual = 0;
 
 	return pagina;
+}
+
+void freePaginaResultados(PAGINA_RESULTADOS pagina, void (*funcaofreeItem)() ){
+	int i, tamanho = getTamanhoLista(pagina);
+	for(i = 0; i < tamanho; i++){
+		funcaofreeItem(getnElemento(pagina,i));
+	}
+
+	free(getListagem(pagina));
+	free(pagina);
 }
 
 void estadoPaginacao(PAGINA_RESULTADOS pagina){
@@ -146,11 +182,25 @@ void calcularUltimaPag(PAGINA_RESULTADOS pagina){
 	int nrPagina = calcularNrPaginasInteiras(getIndiceAtual(pagina), getNrElementosPorPagina(pagina));
 	int nrElementosUltimaPag = calcularNrElementosUltimaPagina(getIndiceAtual(pagina), getNrElementosPorPagina(pagina));
 
-	if(nrElementosUltimaPag == 0) //?
+	if(nrElementosUltimaPag == 0){ 
+		nrPagina--;
+		setPaginaAtual(pagina, nrPagina);
+		setPosicaoInicial(pagina, nrPagina * getNrElementosPorPagina(pagina));
+		setPosicaoFinal(pagina, getPosicaoInicial(pagina) +  getNrElementosPorPagina(pagina) - 1);
+		setPosicaoAtual(pagina, nrPagina * getNrElementosPorPagina(pagina));
+	}else{
+		setPaginaAtual(pagina, nrPagina);
+		setPosicaoInicial(pagina, nrPagina * getNrElementosPorPagina(pagina));
+		setPosicaoFinal(pagina, getPosicaoInicial(pagina) +  nrElementosUltimaPag - 1);
+		setPosicaoAtual(pagina, nrPagina * getNrElementosPorPagina(pagina));
+	}
+}
 
-	setPaginaAtual(pagina, nrPagina);
-	setPosicaoInicial(pagina, nrPagina * getNrElementosPorPagina(pagina));
-	setPosicaoFinal(pagina, getPosicaoInicial(pagina) +  nrElementosUltimaPag - 1);
+void calcularPrimeiraPag(PAGINA_RESULTADOS pagina){
+	setPaginaAtual(pagina, 0);
+	setPosicaoInicial(pagina, 0);
+	setPosicaoFinal(pagina, getNrElementosPorPagina(pagina)-1);
+	setPosicaoAtual(pagina, 0);
 }
 
 int calcularNrPaginasTotal(PAGINA_RESULTADOS pagina){
@@ -162,8 +212,13 @@ int calcularNrPaginasTotal(PAGINA_RESULTADOS pagina){
 int virarPagina(PAGINA_RESULTADOS pagina){
 	int max = getIndiceAtual(pagina);
 	int nrElemPorPag = getNrElementosPorPagina(pagina);
-	// int posicaoInic = getPosicaoInicial(pagina);
+	int posicaoInic = getPosicaoInicial(pagina);
 	int posicaoFin = getPosicaoFinal(pagina);
+
+	if(getPaginaAtual(pagina) >= getNrPaginasTotal(pagina)-1){
+		setPosicaoAtual(pagina, posicaoInic);
+		return 0;
+	}
 
 	if (posicaoFin + nrElemPorPag >= max){ //chegamos ao fim
 		int nrElementosUltimaPag = calcularNrElementosUltimaPagina(getIndiceAtual(pagina), getNrElementosPorPagina(pagina));
@@ -173,6 +228,22 @@ int virarPagina(PAGINA_RESULTADOS pagina){
 		incPaginaAtual(pagina);
 		setPosicaoInicial(pagina, posicaoFin + 1);
 		setPosicaoFinal(pagina, posicaoFin + nrElemPorPag);
+		setPosicaoAtual(pagina, posicaoFin + 1);
+		return getPosicaoInicial(pagina);
+	}
+}
+
+int paginaParaTras(PAGINA_RESULTADOS pagina){
+	int nrElemPorPag = getNrElementosPorPagina(pagina);
+	int posicaoInic = getPosicaoInicial(pagina);
+	if(getPaginaAtual(pagina) == 0){
+			setPosicaoAtual(pagina, 0);
+			return 0; //ja estamos na primeira pagina
+	}else{
+		decPaginaAtual(pagina);
+		setPosicaoInicial(pagina, posicaoInic - nrElemPorPag);
+		setPosicaoFinal(pagina, posicaoInic-1);
+		setPosicaoAtual(pagina, posicaoInic - nrElemPorPag);
 		return getPosicaoInicial(pagina);
 	}
 }
@@ -183,10 +254,12 @@ void posicoesInit(PAGINA_RESULTADOS pagina){
 
 	setPosicaoInicial(pagina, nrpag * elemPorPag);
 	setPosicaoFinal(pagina, getPosicaoInicial(pagina) + elemPorPag - 1);
+	setPosicaoAtual(pagina, nrpag * elemPorPag);
+
+	setNrPaginasTotal(pagina, calcularNrPaginasTotal(pagina));
 }
 
-
-void imprimirElementos(PAGINA_RESULTADOS pagina, void (*funcaoImpressao)() ){
+void imprimirPagina(PAGINA_RESULTADOS pagina, void (*funcaoImpressao)() ){
 	int i = getPosicaoInicial(pagina);
 	int fim = getPosicaoFinal(pagina);
 
@@ -196,6 +269,16 @@ void imprimirElementos(PAGINA_RESULTADOS pagina, void (*funcaoImpressao)() ){
 	}
 }
 
+void* getElementoAtual(PAGINA_RESULTADOS pagina){
+	int p = getPosicaoAtual(pagina);
+	int fim = getPosicaoFinal(pagina);
+
+	if( p  <= fim ){
+		return getnElemento(pagina, incPosicaoAtual(pagina));
+	}else{
+		return NULL;
+	}
+}
 
 void percorrerPaginaResultados(PAGINA_RESULTADOS pagina, int nrPag, int elemPorPag, void (*funcaoImpressao)() ){
 	setNrElementosPorPagina(pagina, elemPorPag);
@@ -204,7 +287,33 @@ void percorrerPaginaResultados(PAGINA_RESULTADOS pagina, int nrPag, int elemPorP
 	while(scanf("%d",&nrPag) != 0){
 		printf("\tTOTAL PAG: %d\n", calcularNrPaginasTotal(pagina));
 		estadoPaginacao(pagina);
-		imprimirElementos(pagina, funcaoImpressao);
+		imprimirPagina(pagina, funcaoImpressao);
 		virarPagina(pagina);
 	}
+}
+
+// n = tamanho
+static void quicksort(void **resultados, int (*f_comparacao)() , int n, void *param){
+    int i, j;
+    void *p, *temp;
+    
+    if (n < 2) return;
+    p = resultados[n/2];
+    
+    for (i = 0, j = n - 1;; i++, j--) {
+        
+        while(f_comparacao(resultados[i],p, param) < 0) i++;
+        while(f_comparacao(p,resultados[j], param) < 0) j--;
+        if (i >= j) break;
+        
+        temp = resultados[i];
+        resultados[i] = resultados[j];
+        resultados[j] = temp;
+    }
+    quicksort(resultados,f_comparacao, i, param);
+    quicksort(resultados+i,f_comparacao, n - i, param);
+}
+
+void ordenarResultadosLista(PAGINA_RESULTADOS pagina, int (*funcaoComparacao)() ){
+	quicksort(getListagem(pagina), funcaoComparacao, getIndiceAtual(pagina), NULL);
 }
