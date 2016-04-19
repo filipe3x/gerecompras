08b191a052c3b88ca900b04388160ae3e2bc3dcd
Filipe Marques <filipemarques@Filipes-MBP.lan>
2016-04-19 19:14:06 +0100

    menus para as queries

--- menu.c
+++ menu.c
@@ -1,64 +1,205 @@
-#define PBSTR "||||||||||||||||||||||||||||||"
-#define PBWIDTH 30
+/*
+ *  *  * Filipe Marques
+ *  *  * Laboratórios Informática III, Universidade do Minho, 2016
+ *  *  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdbool.h>
 
-void printProgress (double percentage)
-{
-    int val = (int) (percentage * 100);
-    int lpad = (int) (percentage * PBWIDTH);
-    int rpad = PBWIDTH - lpad;
-    printf ("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
-    fflush (stdout);
+#include "venda.h"
+#include "menu.h"
+#include "navegacao.h"
+#include "gestao_filiais.h"
+
+/* QUERY 2 */
+
+void query02_funcaoImpressao(int pos, void *s){
+	printf("│ │ %5d │ %8s │                                   │\n", pos, (char *) s);
 }
 
-void menu(){
-    char option;
-    bool isRunning = true;
+void query02_imprimirCabecalho(){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> CATÁLOGOS >> QUERY 2 │░░░░░░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Listar clientes começados pela letra...                │\n");
+	printf("│ ╭───────────────────────────────────────────────╮      │\n");
+	printf("│ │ [1] CATÁLOGOS | [2] Menu Principal | [3] Sair │      │\n");
+	printf("│ ╰───────────────────────────────────────────────╯      │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
 
-    while(isRunning==true){
-        system("clear");
-        fflush(stdin);
-        printf("\n[1]CATÁLOGOS"
-             "\n[2]FATURAÇÃO"
-             "\n[3]GESTÃO DE FILIAIS"
-             "\n[x]Sair"
-             "\nInserir opção > ");
+void query02_imprimirCabecalho_2(char letra, double tempo){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> CATÁLOGOS >> QUERY 2 │░░░░░░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Mostrando clientes começados por %c (em %.5f seg.)   │\n",letra,tempo);
+	printf("│ ╭───────────────────────────────────────────────╮      │\n");
+	printf("│ │ [1] CATÁLOGOS | [2] Menu Principal | [3] Sair │      │\n");
+	printf("│ ╰───────────────────────────────────────────────╯      │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
 
-        option = getchar();
+void query02_imprimirInfo(int paginaAtual, int paginasTotais, int posIni, int posFin, int nrResultadosTotal){
+	printf("│ Pág. %05d/%05d # %06d-%06d de %06d resultados  │\n", paginaAtual+1, paginasTotais-1, posIni+1, posFin+1, nrResultadosTotal);
+	printf("│ ┌───────┬──────────┐                                   │\n");
+	printf("│ │     # │  Código  │                                   │\n");
+}
 
-        putchar('\n');
+/* QUERY 5 */
 
-puts(
-"\n┌─────┬────────┐"
-"\n|  #  | Codigo |"
-"\n├─────┼────────┤"
-"\n│  1  │  100   │"
-"\n│  2  │  500   │"
-"\n└─────┴────────┘");
+void query02_imprimirRodape(){
+	printf("│ ╰───────┴──────────╯                                   │\n");
+	printf("│<<[4]   <[5]  < # >  [6]>   [7]>> || Opção > ");
+}
 
-        putchar('\n');
+void query05_imprimirCabecalho(){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 5 │░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Nº produtos comprados pelo cliente...                  │\n");
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
 
-        switch(option)
-        {
-            case '1':
-                     //TO DO CODE
-                     break;
-            case '2':
-                     //TO DO CODE
-                     break;
-            case '3':
-                     //TO DO CODE
-                     break;
-            //...
-            case 'x':
-                     //Exits the system
-                     isRunning = false;
-                     return ;
-            default :
-                     //User enters wrong input
-                     //TO DO CODE
-                     isRunning = false;
-                     break;
-        }
-    }
-    return ;
+void query05_imprimirCabecalho_2(String codigo, double tempo){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 5 │░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Nº produtos comprados pelo cliente %7s (%.4f seg)│\n",codigo,tempo);
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
+
+void query05_imprimirInfo(int nrFiliais){
+	int f;
+	printf("│    MÊS    │");
+
+	f = 1;
+	while(f <= nrFiliais){
+		printf(" Filial %d │",f);
+		f++;
+	}
+
+	printf("           │\n");
+}
+
+void query05_funcaoImpressao(void** dados, const String mes, int nrFiliais){
+	int f;
+	printf("│ %9s │", mes);
+
+	f = 0;
+	while(f < nrFiliais){
+		printf(" %8ld │",*(long*)dados[f]);
+		f++;
+	}
+
+	printf("           │\n");
+}
+
+
+void query05_imprimirRodape(int nrFiliais){
+	int f;
+	printf("│───────────╯");
+
+	f = 1;
+	while(f <= nrFiliais){
+		printf("──────────╯");
+		f++;
+	}
+
+	printf("           │\n");
+	printf("│ Opção > ");
+}
+
+/* QUERY 9 */
+
+void query09_imprimirCabecalho(){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 9 │░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Produtos mais comprados pelo cliente...                │\n");
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
+
+void query09_imprimirCabecalho_2(String codigo, double tempo){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 9 │░░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Produtos mais comprados pelo cliente %6s (%.4f s) │\n",codigo,tempo);
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
+
+void query09_imprimirInfo(int paginaAtual, int paginasTotais, int posIni, int posFin, int nrResultadosTotal){
+	printf("│ Pág. %05d/%05d # %06d-%06d de %06d resultados  │\n", paginaAtual+1, paginasTotais-1, posIni+1, posFin+1, nrResultadosTotal);
+	printf("│ ┌───────┬──────────┬──────────┐                        │\n");
+	printf("│ │     # │  Código  │Quantidade│                        │\n");
+}
+
+void query09_funcaoImpressao(int pos, void *produto){
+	printf("│ │ %5d │  %6s  │ %9d│                        │\n", pos, getCodProduto_registoProduto(produto), getQuantidade_registoProduto(produto));
+}
+
+void query09_imprimirRodape(){
+	printf("│ ╰───────┴──────────┴──────────╯                        │\n");
+	printf("│<<[4]   <[5]  < # >  [6]>   [7]>> || Opção > ");
+}
+
+/* QUERY 11 */
+
+void query11_imprimirCabecalho(){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 11 │░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Produtos com mais gastos pelo cliente...               │\n");
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
+
+void query11_imprimirCabecalho_2(String codigo, double tempo){
+	printf("\033[2J\033[1;1H");
+	printf("╭────────────────────────────────────────────────────────╮\n");
+	printf("│GERECOMPRAS >> GESTÃO FILIAIS >> QUERY 11 │░░░░░░░░░░░░░│\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+	printf("│ Ranking produtos onde %6s mais gastou (%.4f s)    │\n",codigo,tempo);
+	printf("│ ╭────────────────────────────────────────────────────╮ │\n");
+	printf("│ │ [1] GESTÃO FILIAIS | [2] Menu Principal | [3] Sair │ │\n");
+	printf("│ ╰────────────────────────────────────────────────────╯ │\n");
+	printf("│════════════════════════════════════════════════════════│\n");
+}
+
+void query11_imprimirInfo(int paginaAtual, int paginasTotais, int posIni, int posFin, int nrResultadosTotal){
+	printf("│ Pág. %05d/%05d # %06d-%06d de %06d resultados  │\n", paginaAtual+1, paginasTotais-1, posIni+1, posFin+1, nrResultadosTotal);
+	printf("│ ┌───────┬──────────┬───────────────┐                   │\n");
+	printf("│ │     # │  Código  │    Gastos     │                   │\n");
+}
+
+void query11_funcaoImpressao(int pos, void *produto){
+	printf("│ │ %5d │  %6s  │ %12.2f  │                   │\n", pos, getCodProduto_registoProduto(produto), getPreco_registoProduto(produto));
+}
+
+void query11_imprimirRodape(){
+	printf("│ ╰───────┴──────────┴───────────────╯                   │\n");
+	printf("│<<[4]   <[5]  < # >  [6]>   [7]>> || Opção > ");
 }
