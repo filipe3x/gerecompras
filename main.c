50336d3052a337acf688bf8b1cd59bdb9950275b
Filipe Marques <filipemarques@Filipes-MBP.lan>
2016-04-20 04:03:16 +0100

    organizacao da main

--- main.c
+++ main.c
@@ -9,57 +9,43 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define NRFILIAIS 3
-
 #include "avl.h"
 #include "venda.h"
-#include "menu.h"
 #include "navegacao.h"
-#include "catalogo_clientes.h"
 #include "faturacao.h"
 #include "gestao_filiais.h"
+#include "catalogo_clientes.h"
+#include "catalogo_produtos.h"
 #include "leitura_ficheiros.h"
 #include "queries.h"
-
-//nr clientes validos: 16 384 distintos
-//nr produtos validos: 171 008 distintos
-//nr vendas validos: 891 108
+#include "menu.h"
 
 int main(int argc, char const *argv[]){
-	clock_t tini, tfin;
-	tini = clock();
-
 	CATALOGO_CLIENTES catalogoClientes = catalogoClientesInit();
-	abrirFicheiroClientes(catalogoClientes, "Clientes.txt");
-	// query02_catalogoClientes(catalogoClientes);
-
+	CATALOGO_PRODUTOS catalogoProdutos = catalogoProdutosInit();
 	MODULO_GESTAO_FILIAIS moduloGestao = moduloGestaoFiliaisInit();
 	MODULO_FATURACAO moduloFaturacao = moduloFaturacaoInit();
-	abrirFicheiroVendas("Vendas_100K.txt", moduloGestao, moduloFaturacao);
 
+	abrirFicheiroClientes("Clientes.txt", catalogoClientes);
+	abrirFicheiroProdutos("Produtos.txt", catalogoProdutos);
+	abrirFicheiroVendas("Vendas_1M.txt", moduloGestao, moduloFaturacao, catalogoProdutos, catalogoClientes);
+
+	// query02_catalogoClientes(catalogoClientes);
+	// query02_catalogoProdutos(catalogoClientes);
 	// query05_nrProdutosCompradosPeloCliente(moduloGestao, catalogoClientes);
 	// query09_listaProdutosClienteMaisComprou(moduloGestao, catalogoClientes);
 	// query11_topProdutosClienteGastouDinheiro(moduloGestao, catalogoClientes);
+	// query03_quantidadeFaturacaoProduto_filial(moduloGestao, catalogoProdutos);
+	// query03_quantidadeFaturacaoProduto_global(moduloGestao, catalogoProdutos);
+
+	menu_principal();
+
+	printf("faturacao %f\n", calculaFaturacaoProduto_global(moduloGestao, "NR1091", VENDA_NORMAL) + 
+		calculaFaturacaoProduto_global(moduloGestao, "NR1091", VENDA_PROMOCIONAL));
 
 	freeModuloGestaoFiliais(moduloGestao);
 	freeModuloFaturacao(moduloFaturacao);
 	freeCatalogoClientes(catalogoClientes);
-
-	tfin = clock();
-	// printf("Leitura feita em %.3f segundos.\n ", (double) (tfin - tini)/CLOCKS_PER_SEC);
+	freeCatalogoProdutos(catalogoProdutos);
 	return(EXIT_SUCCESS);
-}
-
-int vendasMain(){
-	MODULO_GESTAO_FILIAIS moduloGestao = moduloGestaoFiliaisInit();
-	MODULO_FATURACAO moduloFaturacao = moduloFaturacaoInit();
-
-	abrirFicheiroVendas("Vendas_teste.txt", moduloGestao, moduloFaturacao);
-
-	// travessiaFaturacao(moduloFaturacao);
-
-	freeModuloGestaoFiliais(moduloGestao);
-	freeModuloFaturacao(moduloFaturacao);
-
-	return (EXIT_SUCCESS);
 }
