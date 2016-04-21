7eba1171227f3ee3c5a1833bddb67b261dd7a33c
Filipe Marques <filipemarques@Filipes-MBP.lan>
2016-04-22 00:45:11 +0100

    variaval extern STATUS, apagar comentarios

--- leitura_ficheiros.h
+++ leitura_ficheiros.h
@@ -10,9 +10,4 @@ extern int STATUS;
 void argumentosMain(int, const char **);
 int abrirFicheirosInit(CATALOGO_CLIENTES,CATALOGO_PRODUTOS,MODULO_GESTAO_FILIAIS,MODULO_FATURACAO);
 
-// void abrirFicheiroVendas(String nomeFicheiro, MODULO_GESTAO_FILIAIS moduloGestaoFiliais, 
-// 	MODULO_FATURACAO moduloFaturacao, CATALOGO_PRODUTOS catalogoProdutos, CATALOGO_CLIENTES catalogoClientes);
-// void abrirFicheiroClientes(String nomeFicheiro, CATALOGO_CLIENTES catalogo);
-// void abrirFicheiroProdutos(String nomeFicheiro, CATALOGO_PRODUTOS catalogo);
-
 #endif /* LEITURAFICHEIROS */
